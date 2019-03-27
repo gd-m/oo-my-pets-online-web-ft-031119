a6b5cb9a9cd9165bdb851928a8f894fbdf46b79b
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-27 04:20:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
   # code goes here
+  attr_accessor :name, :species
+
+  @@all =[]
+  @@count = 0
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @@count += 1
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    self.species
+  end
 end
